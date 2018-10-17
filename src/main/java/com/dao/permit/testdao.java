7f9e77b6cf14cff1389bac9125ded7bc49b4d54b
Administrator <1187272122@qq.com>
2018-10-17 17:48:19 +0800

    删除多于分支测试

--- src/main/java/com/dao/permit/testdao.java
+++ src/main/java/com/dao/permit/testdao.java
@@ -2,6 +2,6 @@ package com.dao.permit;
 
 public class testdao {
     public static void main(String[] args) {
-        System.out.println("权限个人测试提交");
+        System.out.println("权限个人测试提交！");
     }
 }
