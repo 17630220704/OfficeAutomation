0ad5e71397bf0d22e6df609671fffb6f5eed9d44
LiangChenYang <2766588255@qq.com>
2018-10-17 17:33:15 +0800

    第一次上传

---
+++ src/main/java/com/dao/lcy/lcy.java
@@ -0,0 +1,8 @@
+package com.dao.lcy;
+
+public class lcy {
+    public void sss(){
+        System.out.println("ssss");
+
+    }
+}
--- src/main/java/com/dao/lqp/lqp1.java
+++
@@ -1,8 +0,0 @@
-package com.dao.lqp;
-
-public class lqp1{
-    public void test(){
-        System.out.println("哎呦厉害了？？？");
-        System.out.println("石帅二狗子！！！！！");
-    }
-}
