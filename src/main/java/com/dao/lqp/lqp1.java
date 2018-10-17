edab72bf869d8929d45bfc27e98ec9999169d67d
LuQinPeng <wodefeng0606>
2018-10-17 17:03:37 +0800

    二狗子你厉害了啊！！！！

--- src/main/java/com/dao/lqp/lqp1.java
+++ src/main/java/com/dao/lqp/lqp1.java
@@ -4,5 +4,6 @@ public class lqp1{
     public void test(){
         System.out.println("哎呦厉害了？？？");
         System.out.println("石帅二狗子！！！！！");
+        System.out.println("外日，石帅真是二狗子！！！！！");
     }
 }
