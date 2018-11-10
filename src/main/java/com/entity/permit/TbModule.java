6294c59d3e106f4055b903ea706b86e9ac5b87bd
Administrator <1187272122@qq.com>
2018-11-10 23:14:12 +0800

    Merge remote-tracking branch 'remotes/origin/personnel1'
          # Conflicts:
          #	.idea/artifacts/OfficeAutomation_war_exploded.xml
          #	.idea/dataSources.xml
          合并错误修正

---
+++ src/main/java/com/entity/permit/TbModule.java
@@ -0,0 +1,77 @@
+package com.entity.permit;
+
+
+public class TbModule {
+
+  private long mId;
+  private long mLevel;
+  private String mName;
+  private String mUrl;
+  private long aLevel;
+  private String aName;
+  private String aUrl;
+
+
+  public long getMId() {
+    return mId;
+  }
+
+  public void setMId(long mId) {
+    this.mId = mId;
+  }
+
+
+  public long getMLevel() {
+    return mLevel;
+  }
+
+  public void setMLevel(long mLevel) {
+    this.mLevel = mLevel;
+  }
+
+
+  public String getMName() {
+    return mName;
+  }
+
+  public void setMName(String mName) {
+    this.mName = mName;
+  }
+
+
+  public String getMUrl() {
+    return mUrl;
+  }
+
+  public void setMUrl(String mUrl) {
+    this.mUrl = mUrl;
+  }
+
+
+  public long getALevel() {
+    return aLevel;
+  }
+
+  public void setALevel(long aLevel) {
+    this.aLevel = aLevel;
+  }
+
+
+  public String getAName() {
+    return aName;
+  }
+
+  public void setAName(String aName) {
+    this.aName = aName;
+  }
+
+
+  public String getAUrl() {
+    return aUrl;
+  }
+
+  public void setAUrl(String aUrl) {
+    this.aUrl = aUrl;
+  }
+
+}
